feb31608f5ad1691af94136b3ec54a6c2f1eb540
Agustin Groh <agusgroh@gmail.com>
2021-09-08 07:08:34 -0300

    refactor ejercicio 3 TP4- Se agrego clase abstracta

--- TP4/EJ3/src/Empleado.java
+++ TP4/EJ3/src/Empleado.java
@@ -7,45 +7,37 @@ cantidad de ventas realizadas.
 Implementar un sistema que permita saber cuánto se le debe pagar a un empleado al finalizar
 la semana.
 */
-public class Empleado {
+public abstract class Empleado {
 
     private Persona persona;
-    private double salario;
+   
 
     public Empleado(String nombre, String apellido){
-        this.persona= new Persona(nombre, apellido);
-        this.salario=0;
+        this.persona= new Persona(nombre, apellido);     
     }
     
-    public double getSalarioSemanal(){
-        return salario;
-    }
+    public abstract double getSalarioSemanal();    
 
     public String toString(){
-        return "Nombre: " + persona.getNombre() + " Apellido: " + persona.getApellido();
+        return "Nombre: " + this.persona.getNombre() + " Apellido: " + this.persona.getApellido() + "salario semanal: " + this.getSalarioSemanal();
      }
 
-    public static void salarioEmpleado(Empleado empleado){
-        System.out.println(empleado);
-        System.out.println("Salario semanal: " + empleado.getSalarioSemanal());
 
-    } 
 
     public static void main(String[] args) {
 
-        Empleado empleado1= new Empleado("Agustin", "Groh");
+        
         EmpleadoContratado empleado2= new EmpleadoContratado("Marta", "Cantallops",45000);
-        Empleado.salarioEmpleado(empleado1);
-        Empleado.salarioEmpleado(empleado2);
+        System.out.println(empleado2);
 
         EmpleadoHoraExtra empleado3 = new EmpleadoHoraExtra("Seba","Groh",45000);
         empleado3.setHorasTrabajdas(1);
         empleado3.setMontoExtra(600);
-        Empleado.salarioEmpleado(empleado3);
+        System.out.println(empleado3);
         
 
 
         EmpleadoComision empleado4 = new EmpleadoComision("Omar","Groh",45000,2,1.5);
-        Empleado.salarioEmpleado(empleado4);
+        System.out.println(empleado4);
     }
 }
